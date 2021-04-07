8a4fc25607445751a6320f0920e67403ae4ef88d
pboehm90@gmail.com <pboehm90@gmail.com>
2021-04-07 19:56:04 +0200

    Test

--- synczfs/Syncer/DoSync.cs
+++ synczfs/Syncer/DoSync.cs
@@ -109,7 +109,7 @@ namespace synczfs.Syncer
             catch (ProcessException pex)
             {
                 // Tritt auf wenn das Ziel Dataset noch nicht existiert. Wird ja beim sync angelegt!
-                Logging.GetInstance().Log("Dataset Informationen des Ziels konnten nicht gelesen werden. Zieldataset müsste nicht existieren." + Environment.NewLine + pex.ToString());
+                Logging.GetInstance().Log("Dataset information of the destination could not be read. Target dataset should not exist." + Environment.NewLine + pex.ToString());
             }
             
             return null;
